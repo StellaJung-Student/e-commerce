732846dc81f8a1672a61a199f5dfeba15d7e2086
Stella Jung <sojung@myseneca.ca>
2020-08-10 18:56:04 -0400

    implemented signup

--- src/App.js
+++ src/App.js
@@ -28,17 +28,14 @@ class App extends Component {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot((snapShot) => {
-          this.setState(
-            {
-              currentUser: {
-                id: snapShot.id,
-                ...snapShot.data(),
-              },
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
             },
-            () => {
-              console.log(this.state);
-            }
-          );
+          });
+
+          console.log(this.state);
         });
       }
 
