3397d7baf73d7a11c47a61c9638ddf06fcc42037
Stella Jung <sojung@myseneca.ca>
2020-08-18 15:18:19 -0400

    config

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,17 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-
-const config = {
-  apiKey: 'AIzaSyATWXtvt7I6KmJ7BbKiw0food6LqXA-Q7A',
-  authDomain: 'commerce-db-1e28f.firebaseapp.com',
-  databaseURL: 'https://commerce-db-1e28f.firebaseio.com',
-  projectId: 'commerce-db-1e28f',
-  storageBucket: 'commerce-db-1e28f.appspot.com',
-  messagingSenderId: '375037008240',
-  appId: '1:375037008240:web:717214b67a2fe68a1197b5',
-  measurementId: 'G-Y0B5EBR3EM',
-};
+import { firebaseConfig as config } from '../config';
 
 export const createUserProfileDocument = async (userAuth, addtionalData) => {
   if (!userAuth) return;
