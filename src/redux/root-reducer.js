4b720100d77ba225be7de7ea01fb66beaa0c09b1
Stella Jung <sojung@myseneca.ca>
2020-08-18 15:07:29 -0400

    added cart-icon and cart dropdown

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import userReducer from './user/user.reducer';
+import cartReducer from './cart/cart.reduceer';
 
 export default combineReducers({
   user: userReducer,
+  cart: cartReducer,
 });
