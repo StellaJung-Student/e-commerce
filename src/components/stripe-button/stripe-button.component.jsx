3924a5cdec52d71473ebdd01d7e72336c8c6e3f1
Stella Jung <sojung@myseneca.ca>
2020-08-23 20:17:57 -0400

    implemented test payment with stripe

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import { publishableKey as key } from '../../config';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='Stella Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='http://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={key}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
