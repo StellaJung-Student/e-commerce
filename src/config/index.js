6126808d056c0f85128cc06645243f49c43ebfa5
Stella Jung <sojung@myseneca.ca>
2020-08-27 15:48:00 -0400

    firebase api issue

--- src/config/index.js
+++ src/config/index.js
@@ -9,4 +9,39 @@ export const firebaseConfig = {
   measurementId: process.env.REACT_APP_FIREBASE_measurementId,
 };
 
+// export const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_apiKey.substring(
+//     1,
+//     process.env.REACT_APP_FIREBASE_apiKey.length - 2
+//   ),
+//   authDomain: process.env.REACT_APP_FIREBASE_authDomain.substring(
+//     1,
+//     process.env.REACT_APP_FIREBASE_authDomain.length - 2
+//   ),
+//   databaseURL: process.env.REACT_APP_FIREBASE_databaseURL.substring(
+//     1,
+//     process.env.REACT_APP_FIREBASE_databaseURL.length - 2
+//   ),
+//   projectId: process.env.REACT_APP_FIREBASE_projectId.substring(
+//     1,
+//     process.env.REACT_APP_FIREBASE_projectId.length - 2
+//   ),
+//   storageBucket: process.env.REACT_APP_FIREBASE_storageBucket.substring(
+//     1,
+//     process.env.REACT_APP_FIREBASE_storageBucket.length - 2
+//   ),
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_messagingSenderId.substring(
+//     1,
+//     process.env.REACT_APP_FIREBASE_messagingSenderId.length - 2
+//   ),
+//   appId: process.env.REACT_APP_FIREBASE_appId.substring(
+//     1,
+//     process.env.REACT_APP_FIREBASE_appId.length - 2
+//   ),
+//   measurementId: process.env.REACT_APP_FIREBASE_measurementId.substring(
+//     1,
+//     process.env.REACT_APP_FIREBASE_measurementId.length - 2
+//   ),
+// };
+
 export const publishableKey = process.env.REACT_APP_STRIPE_apiKey;
