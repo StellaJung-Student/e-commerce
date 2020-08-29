51fd646b296661847be450f2d500007a955d99df
Stella Jung <sojung@myseneca.ca>
2020-08-28 22:48:04 -0400

    programatically added shop data into firestore - must be deleted added code after adding it once

--- src/App.js
+++ src/App.js
@@ -11,9 +11,14 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import {
+  auth,
+  createUserProfileDocument,
+  addCollectionAndDocuments,
+} from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
+import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
 
 class App extends Component {
   constructor() {
@@ -27,7 +32,7 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
+    const { setCurrentUser, collectionArray } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
@@ -44,6 +49,10 @@ class App extends Component {
       }
 
       setCurrentUser(userAuth);
+      addCollectionAndDocuments(
+        'collections',
+        collectionArray.map(({ title, items }) => ({ title, items }))
+      );
     });
   }
 
@@ -77,6 +86,7 @@ class App extends Component {
 }
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
+  collectionArray: selectCollectionsForPreview,
 });
 
 const mapDispatchToProps = (dispatch) => ({
